be4e6cd35c30e11bff154acdf8d0fb57fc092620
jaechang desktop <jaechang@postech.ac.kr>
2016-11-06 12:48:43 +0900

    문 추가하고 이것저것

---
+++ Koo/Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    private float horiz, enter;
+
+
+    // Use this for initialization
+    void Start () {
+        horiz = 0;enter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (horiz != Input.GetAxis("Horizontal"))
+        {
+            horiz = Input.GetAxis("Horizontal");
+            if (horiz != 0)
+            {
+                if (horiz == 1.0f) GameObject.Find("Cover").GetComponent<DoorControl>().moveRight();
+            }
+        }
+
+        if (enter != Input.GetAxis("Enter"))
+        {
+            enter = Input.GetAxis("Enter");
+            if (enter != 0)
+            {
+                if (enter == 1.0f) GameObject.Find("Main Camera").GetComponent<CamControl>().ZoomIn();
+            }
+        }
+        
+    }
+}
